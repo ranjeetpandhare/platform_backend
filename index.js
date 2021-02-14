e9718868362f86d405bb684a53dd6e138dbe2050
ranjeet <jeetpandhare99@gmail.com>
2021-02-14 16:54:18 +0530

    change

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ app.use('/api/v1/answer', answerRoutes);
 
 // set port, listen for requests
 app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log("Server is running on port");
 });
